06e1ea8b6e5e69d0d508138160c632f6f6a89117
simtop-dev <64870423+simtop-dev@users.noreply.github.com>
2024-09-19 16:04:25 +0100

    Update main.tf
    Updates to security group

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_security_group" "blog" {
     security_group_id = aws_security_group.blog.id
   }
 
-   resource "aws_security_group_rule" "blog_http_in" {
+   resource "aws_security_group_rule" "blog_https_in" {
     type = "ingress"
     from_port = 443
     to_port = 443
@@ -52,7 +52,7 @@ resource "aws_security_group" "blog" {
     security_group_id = aws_security_group.blog.id
   }
 
-   resource "aws_security_group_rule" "blog_http_in" {
+   resource "aws_security_group_rule" "blog_everything_out" {
     type = "egress"
     from_port = 0
     to_port = 0
