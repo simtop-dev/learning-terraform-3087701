694b13663eef132de62461d17f1b4c47fe7d1723
simtop-dev <64870423+simtop-dev@users.noreply.github.com>
2024-09-19 16:00:51 +0100

    Update main.tf
    Revised security groups updates

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "blog" {
   vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
 }
 
@@ -32,7 +32,7 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
   }
 
-  resource "aws_security_group_rule@ @blog_http_in" {
+  resource "aws_security_group_rule" "blog_http_in" {
     type = "ingress"
     from_port = 80
     to_port = 80
@@ -42,7 +42,7 @@ resource "aws_security_group" "blog" {
     security_group_id = aws_security_group.blog.id
   }
 
-   resource "aws_security_group_rule@ @blog_http_in" {
+   resource "aws_security_group_rule" "blog_http_in" {
     type = "ingress"
     from_port = 443
     to_port = 443
@@ -52,7 +52,7 @@ resource "aws_security_group" "blog" {
     security_group_id = aws_security_group.blog.id
   }
 
-   resource "aws_security_group_rule@ @blog_http_in" {
+   resource "aws_security_group_rule" "blog_http_in" {
     type = "egress"
     from_port = 0
     to_port = 0
