f1ecac399b3e8d1f02be5a0cfc8bb7ff4af35e61
simtop-dev <64870423+simtop-dev@users.noreply.github.com>
2024-09-20 17:10:21 +0100

    Removed HTTP listeners

--- main.tf
+++ main.tf
@@ -71,13 +71,13 @@ module "alb" {
     }
   ]
 
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
+#  http_tcp_listeners = [
+#    {
+#     port               = 80
+#     protocol           = "HTTP"
+#    target_group_index = 0
+#   }
+#  ]
 
   tags = {
     Environment = "dev"
