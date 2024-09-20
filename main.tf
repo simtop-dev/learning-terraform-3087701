9b691203e36ced0b2bef979d6fc5f5ee416794c8
simtop-dev <64870423+simtop-dev@users.noreply.github.com>
2024-09-20 17:04:01 +0100

    updates to load balancer

--- main.tf
+++ main.tf
@@ -49,22 +49,26 @@ module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog__vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
 
-  target_groups = {
-    ex-instance = {
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
+  target_groups = [
       name_prefix      = "blog"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
       targets = {
             my_target = {
-            target_id        = aws_instance.blog.id
+            target_id  = aws_instance.blog.id
             port = 80
         }
-  }
+      }
+    }
+  ]
 
   http_tcp_listeners = [
     {
