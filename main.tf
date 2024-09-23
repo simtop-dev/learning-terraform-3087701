9d02698d2d80fa2accd5eb8ccb4752a71e33499e
simtop-dev <64870423+simtop-dev@users.noreply.github.com>
2024-09-23 17:53:24 +0100

    Update to listeners

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ module "alb" {
 
   target_groups = [
     {
-      name_prefix       = "blog-"
+      name_prefix       = "blog"
       backend_protocol  = "HTTP"
       backend_port      = 80
       target_type       = "instance" 
