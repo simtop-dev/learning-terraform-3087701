ce61d51c86bd5beb965a27b5b23175b8ff238451
simtop-dev <64870423+simtop-dev@users.noreply.github.com>
2024-09-24 15:35:28 +0100

    update target group with backend port

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ module "blog_alb" {
     {
       name_prefix      = "blog-"
       protocol         = "HTTP"
-      port             = 80
+      backend_port     = 80
       target_type      = "instance"
         }
   ]
