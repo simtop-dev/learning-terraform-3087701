5df36e57722bf6b04a5aae261feb3d3ae0b83487
simtop-dev <64870423+simtop-dev@users.noreply.github.com>
2024-09-24 15:44:12 +0100

    Update ALB backend protocol

--- main.tf
+++ main.tf
@@ -59,8 +59,8 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
         }
