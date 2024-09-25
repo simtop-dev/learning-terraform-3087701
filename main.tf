6d12e95b47a5b8d3629f5c25a78e49f8f736bf1d
simtop-dev <64870423+simtop-dev@users.noreply.github.com>
2024-09-25 13:21:59 +0100

    refactor to use variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [[var.ami_filer.name]
   }
 
   filter {
@@ -11,7 +11,7 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner]
 }
 
 data "aws_vpc" "default" {
@@ -20,16 +20,16 @@ data "aws_vpc" "default" {
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = "${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"
+
+  enable_nat_gateway = true
 
     tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -37,10 +37,10 @@ module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
 
-  name = "blog"
+  name = "${var.environment.name}-blog"
 
-  min_size = 1
-  max_size = 2
+  min_size = var.asg_min_size
+  max_size = var.asg_max_size
   
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
@@ -53,7 +53,7 @@ module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name    = "blog-alb"
+  name    = "${var.environment.name}-blog-alb"
 
   load_balancer_type = "application"
 
@@ -63,7 +63,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = var.environment.name
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -79,7 +79,7 @@ module "blog_alb" {
  ] 
 
  tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -88,7 +88,7 @@ module "blog_sg" {
   version = "5.2.0"
  
   vpc_id = module.blog_vpc.vpc_id
-  name = "blog"
+  name = "${var.environment.name}-blog"
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules       = ["all-all"]
