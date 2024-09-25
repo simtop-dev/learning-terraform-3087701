48b6caad002f2bad948bcf867b82d3ae0885e2dd
simtop-dev <64870423+simtop-dev@users.noreply.github.com>
2024-09-25 10:23:00 +0100

    update autoscaler vpc_zone_indentifier

--- main.tf
+++ main.tf
@@ -45,20 +45,19 @@ module "blog_vpc" {
 #  }
 #}
 
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "8.0.0"
 
   name = "blog"
+
   min_size = 1
   max_size = 2
-
-  vpc_zone_indentifier  = module_blog_vpc_public_subnets
+  vpc_zone_indentifier  = module.blog_vpc_public_subnets
   target_groups_arns    = module.blog_alb.target_group_arns
   security_groups       = [module.blog_sg.security_group_id]
-
-  image_id           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  image_id              = data.aws_ami.app_ami.id
+  instance_type         = var.instance_type
  }
 
 module "blog_alb" {
